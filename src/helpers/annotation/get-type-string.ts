4418724966c8841701b6e056652000bfc02013d6
Omar <omarkmu@gmail.com>
2025-04-01 17:19:13 -0500

    Wrap function types in parentheses

--- src/helpers/annotation/get-type-string.ts
+++ src/helpers/annotation/get-type-string.ts
@@ -11,7 +11,9 @@ export const getTypeString = (
 
     const type = types.size > 0 ? [...types].join(' | ') : 'unknown'
     if (nullable) {
-        return type.includes('|') ? `(${type})?` : `${type}?`
+        return type.includes('|') || type.startsWith('fun(')
+            ? `(${type})?`
+            : `${type}?`
     }
 
     return type
